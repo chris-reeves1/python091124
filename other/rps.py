d3e274ac3a4f9574ac541032eb4c779135d4023d
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-12-12 09:35:45 +0000

    2

---
+++ other/rps.py
@@ -0,0 +1,3 @@
+# app to play rps against the computer. A function to handle the game logic and 
+# a loop to handle rounds. keep track of games played, user wins and computer wins.
+# Use import random (random choice) to choose the computer moves. 
